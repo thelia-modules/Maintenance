63edf45f0f687f85df18c1f266d5cab4da3000fa
Alexandre Nozière <anoziere@openstudio.fr>
2026-06-30 12:40:55 +0200

    fix(i18n): add French translations for maintenance back-office labels
    Create I18n/backOffice/default/fr_FR.php and en_US.php with all 11
    keys used in module-configuration.html.twig under the
    maintenance.bo.default domain. Without these files, the BO showed
    English strings regardless of locale.

---
+++ I18n/backOffice/default/en_US.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'Maintenance module configuration' => 'Maintenance module configuration',
+    'Disable maintenance' => 'Disable maintenance',
+    'Enable maintenance' => 'Enable maintenance',
+    'The file web/index.php is not writable please change the permissions or the module will not work' => 'The file web/index.php is not writable, please change the permissions or the module will not work.',
+    'The directory web is not writable please change the permissions or the module will not work' => 'The directory web is not writable, please change the permissions or the module will not work.',
+    'Title' => 'Title',
+    'Message' => 'Message',
+    'Background color' => 'Background color',
+    'Font color' => 'Font color',
+    'Link color' => 'Link color',
+    'Save for preview' => 'Save for preview',
+];
---
+++ I18n/backOffice/default/fr_FR.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'Maintenance module configuration' => 'Configuration du module Maintenance',
+    'Disable maintenance' => 'Désactiver la maintenance',
+    'Enable maintenance' => 'Activer la maintenance',
+    'The file web/index.php is not writable please change the permissions or the module will not work' => 'Le fichier web/index.php n\'est pas accessible en écriture. Modifiez les permissions, sinon le module ne fonctionnera pas.',
+    'The directory web is not writable please change the permissions or the module will not work' => 'Le répertoire web n\'est pas accessible en écriture. Modifiez les permissions, sinon le module ne fonctionnera pas.',
+    'Title' => 'Titre',
+    'Message' => 'Message',
+    'Background color' => 'Couleur de fond',
+    'Font color' => 'Couleur du texte',
+    'Link color' => 'Couleur des liens',
+    'Save for preview' => 'Enregistrer pour prévisualiser',
+];
